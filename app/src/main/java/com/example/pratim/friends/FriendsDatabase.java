a2aa917438dcb3ce1a15000293f42e570daba4e3
Pratim Singha <pratim.singha@gmail.com>
2017-01-16 01:20:29 +0530

    Content Provider

--- app/src/main/java/com/example/pratim/friends/FriendsDatabase.java
+++ app/src/main/java/com/example/pratim/friends/FriendsDatabase.java
@@ -1,7 +1,51 @@
 package com.example.pratim.friends;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+
 /**
  * Created by pratim on 08-01-2017.
  */
-public class FriendsDatabase {
+public class FriendsDatabase extends SQLiteOpenHelper{
+    private static final String TAG = FriendsDatabase.class.getSimpleName();
+    private static final String DATABASE_NAME = "friends.db";
+    private static final int DATABASE_VERSION = 2;
+    private final Context mContext;
+
+    interface Tables{
+        String FRIENDS = "friends";
+    }
+
+    public FriendsDatabase(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        mContext = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("CREATE TABLE " + Tables.FRIENDS + " ("
+            + BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+            + FriendsContract.FriendsColumns.FRIENDS_NAME + " TEXT NOT NULL,"
+            + FriendsContract.FriendsColumns.FRIENDS_EMAIL + " TEXT NOT NULL"
+            + FriendsContract.FriendsColumns.FRIENDS_PHONE + " TEXT NOT NULL)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        int version = i;
+        if(version == 1){
+            //Add some extra fields to the database without deleting existing data
+            version = 2;
+        }
+        if(version != DATABASE_VERSION){
+            sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + Tables.FRIENDS);
+            onCreate(sqLiteDatabase);
+        }
+    }
+
+    public static void deleteDatabase(Context context){
+        context.deleteDatabase(DATABASE_NAME);
+    }
 }
--- app/src/main/java/com/example/pratim/friends/FriendsProvider.java
+++ app/src/main/java/com/example/pratim/friends/FriendsProvider.java
@@ -1,7 +1,143 @@
 package com.example.pratim.friends;
 
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.net.Uri;
+import android.os.CancellationSignal;
+import android.provider.BaseColumns;
+import android.support.annotation.Nullable;
+import android.text.TextUtils;
+import android.util.Log;
+
 /**
  * Created by pratim on 08-01-2017.
  */
-public class FriendsProvider {
+public class FriendsProvider extends ContentProvider{
+    private FriendsDatabase mOpenHelper;
+
+    private static String TAG = FriendsProvider.class.getSimpleName();
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+
+    private static final int FRIENDS = 100;
+    private static final int FRIENDS_ID = 101;
+
+    private static UriMatcher buildUriMatcher(){
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority = FriendsContract.CONTENT_AUTHORITY;
+        matcher.addURI(authority,"friends",FRIENDS);
+        matcher.addURI(authority,"friends/*",FRIENDS_ID);
+        return matcher;
+    }
+
+    @Override
+    public boolean onCreate() {
+        mOpenHelper = new FriendsDatabase(getContext());
+        return true;
+    }
+
+    private void deleteDatabase(){
+        mOpenHelper.close();
+        FriendsDatabase.deleteDatabase(getContext());
+        mOpenHelper = new FriendsDatabase(getContext());
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case FRIENDS:
+                return FriendsContract.Friends.CONTENT_TYPE;
+            case FRIENDS_ID:
+                return FriendsContract.Friends.CONTENT_ITEM_TYPE;
+            default:
+                throw new IllegalArgumentException("Unknown Uri: " + uri);
+        }
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues contentValues) {
+        Log.v(TAG, "insert(uri=" + uri + ", values=" + contentValues.toString());
+        final SQLiteDatabase db = mOpenHelper.getReadableDatabase();
+        final int match = sUriMatcher.match(uri);
+        switch (match){
+            case FRIENDS:
+                long recordId = db.insertOrThrow(FriendsDatabase.Tables.FRIENDS,null,contentValues);
+                return FriendsContract.Friends.buildFriendUri(String.valueOf(recordId));
+            default:
+                throw new IllegalArgumentException("Unknown Uri: " + uri);
+        }
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        final SQLiteDatabase db = mOpenHelper.getReadableDatabase();
+        final int match = sUriMatcher.match(uri);
+        SQLiteQueryBuilder queryBuilder = new SQLiteQueryBuilder();
+        queryBuilder.setTables(FriendsDatabase.Tables.FRIENDS);
+
+        switch (match){
+            case FRIENDS:
+                //do nothing
+                break;
+            case FRIENDS_ID:
+                String id = FriendsContract.Friends.getFriendId(uri);
+                queryBuilder.appendWhere(BaseColumns._ID + "=" + id);
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown Uri: " + uri);
+        }
+        Cursor cursor = queryBuilder.query(db, projection, selection, selectionArgs, null, null, sortOrder);
+        return null;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues contentValues, String s, String[] strings) {
+        Log.v(TAG, "update(uri=" + uri + ", values=" + contentValues.toString());
+        final SQLiteDatabase db = mOpenHelper.getReadableDatabase();
+        final int match = sUriMatcher.match(uri);
+
+        String selectionCriteria = s;
+        switch (match){
+            case FRIENDS:
+                //do nothing
+                break;
+            case FRIENDS_ID:
+                String id = FriendsContract.Friends.getFriendId(uri);
+                selectionCriteria = BaseColumns._ID + "=" + id + (!TextUtils.isEmpty(s) ? " AND (" + s + ")" : "");
+                break;
+
+            default:
+                throw new IllegalArgumentException("Unknown Uri: " + uri);
+        }
+        return db.update(FriendsDatabase.Tables.FRIENDS,contentValues,selectionCriteria,strings);
+    }
+
+    @Override
+    public int delete(Uri uri, String s, String[] strings) {
+        Log.v(TAG, "delete(uri=" + uri);
+
+        if(uri.equals(FriendsContract.BASE_CONTENT_URI)){
+            deleteDatabase();
+            return 0;
+        }
+        final SQLiteDatabase db = mOpenHelper.getReadableDatabase();
+        final int match = sUriMatcher.match(uri);
+        switch (match){
+            case FRIENDS:
+                //do nothing
+                break;
+            case FRIENDS_ID:
+                String id = FriendsContract.Friends.getFriendId(uri);
+                String selectionCriteria = BaseColumns._ID + "=" + id + (!TextUtils.isEmpty(s) ? " AND (" + s + ")" : "");
+                return db.delete(FriendsDatabase.Tables.FRIENDS, selectionCriteria, strings);
+
+
+            default:
+                throw new IllegalArgumentException("Unknown Uri: " + uri);
+        }
+    }
 }
